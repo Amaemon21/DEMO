9c26c716822af8e4384cb6141b89a15f0f39963a
Amaemon21 <nik_gavrilov_226@mail.ru>
2025-06-06 14:19:31 +0300

    init

---
+++ WindowsFormsApp1/WindowsFormsApp1/DatabaseConnection.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class DatabaseConnection
+    {
+        private readonly string _connectionString = @"Server=.\SQLEXPRESS;Database=DEMODB;Trusted_Connection=True;";
+
+        private SqlConnection _sqlConnection;
+
+        private void Connection()
+        {
+            _sqlConnection = new SqlConnection(_connectionString);
+            _sqlConnection.Open();
+        }
+
+        private DataTable GetPlayersTable()
+        {
+            Connection();
+
+            DataTable table = new DataTable();
+
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Players", _sqlConnection))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    table.Load(reader);
+                }
+            }
+
+            _sqlConnection.Close();
+            return table;
+        }
+
+
+        public void DisplayPlayers(DataGridView gridView)
+        {
+            DataTable table = GetPlayersTable();
+            gridView.DataSource = table;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1: Form
+    {
+        private DatabaseConnection _dbConnection = new DatabaseConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _dbConnection.DisplayPlayers(dataGridView1);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _dbConnection.DisplayPlayers(dataGridView1);
+        }
+    }
+}
